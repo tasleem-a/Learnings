ea250afa16e8a9e97f965e73588a0c57e65cfe5f
tasleem-a <tasleem.29a@gmail.com>
2024-12-21 23:13:14 +0530

    added 123

--- src/test/java/BaseLibrary.java
+++ src/test/java/BaseLibrary.java
@@ -14,7 +14,7 @@ public class BaseLibrary {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
         AppiumDriver driver;
-        system.out.println("hello");
+        system.out.println("hello123");
         system.out.println("hello2");
         DesiredCapabilities cap=new DesiredCapabilities();
         cap.setCapability("appium:platformName","Android");
